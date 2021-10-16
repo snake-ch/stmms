f195f717addf7324bf7f548babbf01f22734b457
snake-ch <291222536@qq.com>
2021-10-17 05:53:54 +0800

    rename some functions

--- pkg/protocol/rtmp/conn_command.go
+++ pkg/protocol/rtmp/conn_command.go
@@ -7,21 +7,6 @@ import (
 	"gosm/pkg/log"
 )
 
-// ConnInfo net connection information
-type ConnInfo struct {
-	App            string
-	FlashVer       string
-	SwfURL         string
-	TcURL          string
-	Fpad           bool
-	Capabilities   int
-	AudioCodecs    int
-	VideoCodecs    int
-	VideoFunction  int
-	PageURL        string
-	ObjectEncoding int
-}
-
 // OnCommand rtmp command handler
 func (nc *NetConnection) onCommand(streamID uint32, command *Command) error {
 	log.Debug("%0s command: %+v\n", "C -> S", *command)
@@ -37,12 +22,20 @@ func (nc *NetConnection) onCommand(streamID uint32, command *Command) error {
 		case "close":
 		case "createStream":
 			return nc.onCreateStream(command)
-		// commands followed not defined in rtmp_spec, but using stream which id equals 0
+		// command response, see:
+		// https://help.adobe.com/zh_CN/FlashPlatform/reference/actionscript/3/flash/events/NetStatusEvent.html
+		case "_result":
+			fallthrough
+		case "_error":
+			fallthrough
+		case "onStatus":
+			return nc.onResult(command)
+		// commands followed not defined in rtmp-spec-1.0
 		case "releaseStream":
 		case "FCPublish":
-			return nc.fcPublish(command)
+			return nc.onFCPublish(command)
 		case "FCUnpublish":
-			return nc.fcUnpublish(command)
+			return nc.onFCUnpublish(command)
 		case "deleteStream":
 			return nc.onDeleteStream(command)
 		case "getStreamLength":
@@ -61,50 +54,54 @@ func (nc *NetConnection) onCommand(streamID uint32, command *Command) error {
 // OnConnect .
 func (nc *NetConnection) onConnect(command *Command) error {
 	// parse client information
-	nc.info = &ConnInfo{}
-	if property, ok := command.Objects[0].(map[string]interface{}); ok {
-		if app, ok := property["app"]; ok {
+	if obj, ok := command.Objects[0].(map[string]interface{}); ok {
+		if app, ok := obj["app"]; ok {
 			nc.info.App = app.(string)
 		}
-		if flashVer, ok := property["flashVer"]; ok {
+		if flashVer, ok := obj["flashVer"]; ok {
 			nc.info.FlashVer = flashVer.(string)
 		}
-		if swfURL, ok := property["swfUrl"]; ok {
+		if swfURL, ok := obj["swfUrl"]; ok {
 			nc.info.SwfURL = swfURL.(string)
 		}
-		if tcURL, ok := property["tcUrl"]; ok {
-			nc.info.SwfURL = tcURL.(string)
+		if tcURL, ok := obj["tcUrl"]; ok {
+			nc.info.TcURL = tcURL.(string)
 		}
-		if fpad, ok := property["fpad"]; ok {
+		if fpad, ok := obj["fpad"]; ok {
 			nc.info.Fpad = fpad.(bool)
 		}
-		if capabilities, ok := property["capabilities"]; ok {
+		if capabilities, ok := obj["capabilities"]; ok {
 			nc.info.Capabilities = int(capabilities.(float64))
 		}
-		if audioCodecs, ok := property["audioCodecs"]; ok {
+		if audioCodecs, ok := obj["audioCodecs"]; ok {
 			nc.info.AudioCodecs = int(audioCodecs.(float64))
 		}
-		if videoCodecs, ok := property["videoCodecs"]; ok {
+		if videoCodecs, ok := obj["videoCodecs"]; ok {
 			nc.info.VideoCodecs = int(videoCodecs.(float64))
 		}
-		if videoFunction, ok := property["videoFunction"]; ok {
+		if videoFunction, ok := obj["videoFunction"]; ok {
 			nc.info.VideoFunction = int(videoFunction.(float64))
 		}
-		if objectEncoding, ok := property["objectEncoding"]; ok {
+		if pageURL, ok := obj["pageUrl"]; ok {
+			nc.info.PageURL = pageURL.(string)
+		}
+		if objectEncoding, ok := obj["objectEncoding"]; ok {
 			nc.info.ObjectEncoding = int(objectEncoding.(float64))
 		}
 	}
+
+	if nc.info.TcURL == "" {
+		return nc.WriteCommand(SIDNetConnnection, connectReject(command.TransactionID))
+	}
+
 	nc.SetChunkSize(4096)
 	nc.SetWindowAckSize()
 	nc.SetPeerBandwidth(2500000, BindwidthLimitDynamic)
-	return nc.WriteCommand(SIDNetConnnection, new(Command).connectSuccess(command.TransactionID))
+	return nc.WriteCommand(SIDNetConnnection, connectSuccess(command.TransactionID))
 }
 
 // OnCreateStream .
 func (nc *NetConnection) onCreateStream(command *Command) error {
-	if nc.info == nil {
-		return fmt.Errorf("RTMP: net-connection command: %s error, connect server before", command.Name)
-	}
 	// find unused stream id, got 1 usually
 	streamID := uint32(1)
 	for {
@@ -114,7 +111,7 @@ func (nc *NetConnection) onCreateStream(command *Command) error {
 		}
 		streamID = streamID + 1
 	}
-	return nc.WriteCommand(SIDNetConnnection, new(Command).createStreamSuccess(command.TransactionID, streamID))
+	return nc.WriteCommand(SIDNetConnnection, createStreamSuccess(command.TransactionID, streamID))
 }
 
 // FCPublish .
@@ -123,12 +120,12 @@ func (nc *NetConnection) onCreateStream(command *Command) error {
 // to maintain list of all streams or also to force FME to stop publishing.
 // To stop publishing, call "onFCPublish" with an info object with status
 // code set to "NetStream.Publish.BadName".
-func (nc *NetConnection) fcPublish(command *Command) error {
+func (nc *NetConnection) onFCPublish(command *Command) error {
 	return nil
 }
 
 // fcUnpublish FME notifies server script when a stream is unpublished.
-func (nc *NetConnection) fcUnpublish(command *Command) error {
+func (nc *NetConnection) onFCUnpublish(command *Command) error {
 	return nil
 }
 
@@ -144,10 +141,17 @@ func (nc *NetConnection) onDeleteStream(command *Command) error {
 	return nil
 }
 
+// OnResult .
+func (nc *NetConnection) onResult(command *Command) error {
+	return nil
+}
+
 // WriteCommand write rtmp command response
 // @StreamID: 0 -> netConnection, generate by server -> netStream
 func (nc *NetConnection) WriteCommand(streamID uint32, command *Command) error {
 	buf := new(bytes.Buffer)
-	command.WriteTo(buf)
+	if _, err := command.WriteTo(buf); err != nil {
+		return err
+	}
 	return nc.WriteMessage(CommandAmf0, streamID, 0, buf.Bytes())
 }
