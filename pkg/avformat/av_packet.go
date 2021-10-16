db3a2cc3bda27718e82ea5fe046718b66597aaad
snake-ch <291222536@qq.com>
2021-10-17 05:47:34 +0800

    more util functions

--- pkg/avformat/av_packet.go
+++ pkg/avformat/av_packet.go
@@ -1,15 +1,19 @@
 package avformat
 
-import "gosm/pkg/avformat/flv"
+import (
+	"fmt"
+	"gosm/pkg/avformat/flv"
+)
 
 // AV packet type
 const (
 	TypeAudio        = uint8(8)
 	TypeVideo        = uint8(9)
 	TypeMetadataAMF0 = uint8(18)
+	TypeMetadataAMF3 = uint8(15)
 )
 
-// MetaData common metadata, imitates rtmp metadata, see video_file_format_spec_v10.pdf, setion onMetaData
+// MetaData common metadata, same as rtmp metadata, see video_file_format_spec_v10.pdf, setion onMetaData
 type MetaData struct {
 	Server          string      `json:"server,omitempty"`
 	Duration        int         `json:"duration,omitempty"`
@@ -26,13 +30,23 @@ type MetaData struct {
 	Stereo          bool        `json:"stereo,omitempty"`
 }
 
-// AVPacket common packet carries audio/video/metadata, imitates RTMP message
+// AVPacket common packet carries audio/video/metadata, same as RTMP message
 type AVPacket struct {
 	TypeID    uint8
 	Length    uint32
 	Timestamp uint32
 	StreamID  uint32
 	Body      []byte
+}
+
+// IsVideo .
+func (packet *AVPacket) IsVideo() bool {
+	return packet.TypeID == TypeVideo
+}
+
+// IsAudio .
+func (packet *AVPacket) IsAudio() bool {
+	return packet.TypeID == TypeAudio
 }
 
 // IsAVC .
@@ -65,10 +79,28 @@ func (packet *AVPacket) IsAVCKeyframe() bool {
 	return body[0] == flv.AVCKeyFrame<<4|flv.CodevIDAVC && body[1] == flv.AVCNALU
 }
 
+// IsAVCInterframe .
+func (packet *AVPacket) IsAVCInterframe() bool {
+	body := packet.Body
+	return body[0] == flv.AVCInterFrame<<4|flv.CodevIDAVC && body[1] == flv.AVCNALU
+}
+
 // IsHEVCKeyframe .
 func (packet *AVPacket) IsHEVCKeyframe() bool {
 	body := packet.Body
 	return body[0] == flv.AVCKeyFrame<<4|flv.CodevIDHEVC && body[1] == flv.AVCNALU
+}
+
+// IsHEVCInterframe .
+func (packet *AVPacket) IsHEVCInterframe() bool {
+	body := packet.Body
+	return body[0] == flv.AVCInterFrame<<4|flv.CodevIDHEVC && body[1] == flv.AVCNALU
+}
+
+// IsAAC .
+func (packet *AVPacket) IsAAC() bool {
+	body := packet.Body
+	return body[0]>>4 == flv.SoundFormatAAC
 }
 
 // IsAACSeqHeader .
@@ -81,4 +113,10 @@ func (packet *AVPacket) IsAACSeqHeader() bool {
 func (packet *AVPacket) IsAACRaw() bool {
 	body := packet.Body
 	return body[0]>>4 == flv.SoundFormatAAC && body[1] == flv.AACRaw
+}
+
+// String .
+func (packet *AVPacket) String() string {
+	return fmt.Sprintf("{TypeID: %d, Length: %d, TimeStramp: %d, StreamID: %d}",
+		packet.TypeID, packet.Length, packet.Timestamp, packet.StreamID)
 }
