51b24a69656c5cab92305fd2513c2fe5dc434102
snake-ch <291222536@qq.com>
2021-10-17 05:44:58 +0800

    hls support

--- cmd/gosm.go
+++ cmd/gosm.go
@@ -6,17 +6,13 @@ import (
 	"os/signal"
 	"syscall"
 
+	"gosm/pkg/config"
 	"gosm/pkg/live"
 	"gosm/pkg/log"
+	"gosm/pkg/protocol/hls"
 	"gosm/pkg/protocol/httpflv"
 	"gosm/pkg/protocol/rtmp"
-)
-
-const (
-	_LogPrefiex = "[GoSM]"
-	_PortRTMP   = "1935"
-	_PortFlv    = "8088"
-	_Version    = "0.0.1"
+	"gosm/pkg/protocol/rtsp/udp"
 )
 
 func main() {
@@ -26,32 +22,73 @@ func main() {
     | |  __  ___| (___ | \  / |
     | | |_ |/ _ \\___ \| |\/| |
     | |__| | (_) |___) | |  | |
-     \_____|\___/_____/|_|  |_|    version: %s`, _Version+"\n\n")
+     \_____|\___/_____/|_|  |_|    version: %s`, config.Version+"\n\n")
 
-	log.SetLevel(log.LevelDebug)
-	log.SetPrefix(_LogPrefiex)
+	// log level
+	log.SetLevel(config.Global.LogLevel)
 
 	// living room managerment
-	roomMgmt, err := live.NewRoomMgmt()
+	roomMgmt := live.NewRoomMgmt()
 
 	// rtmp server
-	rtmpServer, rtmpCloseFunc, err := rtmp.NewServer("tcp", ":"+_PortRTMP, roomMgmt)
+	rtmpServer, rtmpCloseFunc, err := rtmp.NewServer("tcp", ":"+config.Global.RTMP.Port)
 	if err != nil {
-		log.Fatal("RTMP Server Start Faild:%v", err)
+		log.Fatal("RTMP Server Starts Faild:%v", err)
 	}
-	rtmpServer.Start()
+	rtmpServer.SetObserver(roomMgmt)
+	rtmpServer.Serve()
 
 	// http-flv server
-	flvServer, flvCloseFunc, err := httpflv.NewServer("tcp", ":"+_PortFlv, roomMgmt)
+	flvServer, flvCloseFunc, err := httpflv.NewServer("tcp", ":"+config.Global.HTTPFLV.Port)
 	if err != nil {
-		log.Fatal("HTTP-Flv Server Start Faild:%v", err)
+		log.Fatal("HTTP-Flv Server Starts Faild:%v", err)
 	}
-	flvServer.Start()
+	flvServer.SetObserver(roomMgmt)
+	flvServer.Serve()
 
-	// TODO: hls server
+	// hls server
+	hlsServer, hlsCloseFunc, err := hls.NewServer("tcp", ":"+config.Global.HLS.Port)
+	if err != nil {
+		log.Fatal("HLS Server Starts Faild:%v", err)
+	}
+	hlsServer.Serve()
+
+	// udp server
+	session, err := udp.NewSession("50000")
+	if err != nil {
+		log.Fatal("UDP Server Starts Faild:%v", err)
+	}
+	go session.Serve()
+
+	// udp to rtmp client
+	rtmpClient, err := rtmp.NewClient(config.Global.RTP.Remote)
+	if err != nil {
+		log.Fatal("RTMP Client Initials Faild:%v", err)
+	}
+	if err := rtmpClient.Handshake(); err != nil {
+		log.Fatal("%+v", err)
+	}
+	if err := rtmpClient.Connect(); err != nil {
+		log.Fatal("%+v", err)
+	}
+	if err := rtmpClient.Publish(); err != nil {
+		log.Fatal("%+v", err)
+	}
+
+	go func() {
+		for {
+			avPacket, err := session.ReadAVPacket()
+			if err != nil {
+				return
+			}
+			if err := rtmpClient.WriteAVPacket(avPacket); err != nil {
+				return
+			}
+		}
+	}()
 
 	// Wait for interrupt signal to gracefully shutdown the server.
-	quit := make(chan os.Signal)
+	quit := make(chan os.Signal, 1)
 	// kill (no param) default send syscall.SIGTERM
 	// kill -2 is syscall.SIGINT
 	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
@@ -62,6 +99,7 @@ func main() {
 		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
 			rtmpCloseFunc()
 			flvCloseFunc()
+			hlsCloseFunc()
 			return
 		case syscall.SIGHUP:
 		default:
